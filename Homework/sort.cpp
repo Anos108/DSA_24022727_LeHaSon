b8919c62e89b5a57b1089efeef66c8fbba5f245c
LÊ HÀ SƠN <24022727@vnu.edu.vn>
2025-10-05 17:05:31 +0700

    done

--- Homework/sort.cpp
+++ Homework/sort.cpp
@@ -43,7 +43,7 @@ void insertionSort(vector<int> &arr) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
-        while (j >= 0 && arr[j] > arr[j + 1]) {
+        while (j >= 0 && arr[j] > key) {
             swap(arr[j], arr[j + 1]);
             j--;
         }
