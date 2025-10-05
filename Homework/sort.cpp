3fb83555a2b7416b7eb476d46292978062d32295
LÊ HÀ SƠN <24022727@vnu.edu.vn>
2025-10-05 17:11:52 +0700

    Correct sorting algorithm implementation
    Fix sorting logic by correctly assigning values during swaps.

--- Homework/sort.cpp
+++ Homework/sort.cpp
@@ -44,7 +44,7 @@ void insertionSort(vector<int> &arr) {
         int key = arr[i];
         int j = i - 1;
         while (j >= 0 && arr[j] > arr[j + 1]) {
-            swap(arr[j], arr[j + 1]);
+            arr[j + 1] = arr[j];
             j--;
         }
         arr[j + 1] = key;
