f66d9bb9c50352cc6267204aa444954f9073c4eb
LÊ HÀ SƠN <24022727@vnu.edu.vn>
2025-11-21 12:36:36 +0700

    Add Dijkstra's algorithm implementation and update CMakeLists for week 7

--- week7/prim.cpp
+++ week7/prim.cpp
@@ -1,14 +1,12 @@
 #include "prim.h"
 
-#include <iostream>
-#include <ostream>
 #include <queue>
 #include <vector>
 
 using namespace std;
 
-int findMST(int V, int E, vector<vector<int>> &edges) {
-    vector<vector<int>> adj[V];
+int findMST(int V, int E, vector<vector<int> > &edges) {
+    vector<vector<int> > adj[V];
 
 
     for (int i = 0; i < E; i++) {
@@ -20,7 +18,7 @@ int findMST(int V, int E, vector<vector<int>> &edges) {
     }
 
 
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
 
 
     vector<bool> visited(V, false);
@@ -29,28 +27,28 @@ int findMST(int V, int E, vector<vector<int>> &edges) {
     int res = 0;
 
 
-    pq.push({0, 0});
+    pq.emplace(0, 0);
 
     // Perform Prim's algorithm to find the Minimum Spanning Tree
-    while(!pq.empty()){
+    while (!pq.empty()) {
         auto p = pq.top();
         pq.pop();
 
-        int wt = p.first;  // Weight of the edge
-        int u = p.second;  // Vertex connected to the edge
+        int wt = p.first;
+        int u = p.second;
 
-        if(visited[u] == true){
-            continue;  // Skip if the vertex is already visited
+        if (visited[u] == true) {
+            continue;
         }
 
-        res += wt;  // Add the edge weight to the result
-        visited[u] = true;  // Mark the vertex as visited
+        res += wt;
+        visited[u] = true;
 
-        // Explore the adjacent vertices
-        for(auto v : adj[u]){
+
+        for (auto v: adj[u]) {
             // v[0] represents the vertex and v[1] represents the edge weight
-            if(visited[v[0]] == false){
-                pq.push({v[1], v[0]});  // Add the adjacent edge to the priority queue
+            if (visited[v[0]] == false) {
+                pq.emplace(v[1], v[0]); // Add the adjacent edge to the priority queue
             }
         }
     }
